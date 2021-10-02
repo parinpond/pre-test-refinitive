bc57932c4d8eeb7d14a15fc806493bf2789692c1
parinpond <parinpond.lag@gmail.com>
2021-10-02 23:23:13 +0700

    (feat): Move a star to middle of the box

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { QuestionOneComponent } from './question-one/question-one.component';
+import { QuestionTwoComponent } from './question-two/question-two.component';
 
 
 const routes: Routes = [
   {path:'',component:DashboardComponent},
   {path:'one',component:QuestionOneComponent},
+  {path:'two',component:QuestionTwoComponent},
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,11 +11,13 @@ import {HttpClientModule} from '@angular/common/http';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {DemoMaterialModule} from './material-module';
 import { QuestionOneComponent } from './question-one/question-one.component';
+import { QuestionTwoComponent } from './question-two/question-two.component';
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    QuestionOneComponent
+    QuestionOneComponent,
+    QuestionTwoComponent
   ],
   imports: [
     BrowserModule,
